3938e7299c0dfe1d0dc34b2a4e69d0450f16f650
Irani junior <iranijunior95@gmail.com>
2024-11-16 11:06:24 -0300

    commit: correção bug menu mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 (function() {
     const $btnMenuMobile = document.querySelector('#btn-menu-mobile');
     const $menuMobile = document.querySelector('#menu-mobile');
-    
+
     $btnMenuMobile.addEventListener('click', () => $menuMobile.classList.toggle('active'));
 }());
